f8297da1b0a2d2c963f8ed970d28d47536421ce5
thiago-santanna <uzmaki_thiago@outlook.com>
2023-12-04 09:04:52 -0300

    iniciando projeto

---
+++ src/main/java/com/tsswebapps/pixproxy/PixProxyApplication.java
@@ -0,0 +1,13 @@
+package com.tsswebapps.pixproxy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PixProxyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PixProxyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tsswebapps/pixproxy/PixProxyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tsswebapps.pixproxy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PixProxyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
